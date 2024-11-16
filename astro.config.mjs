4452bc249e2c974754865945cdb4b031ea557d47
Josiah Smith <jcsmithf22@gmail.com>
2024-11-15 20:39:40 -0600

    add anime

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,14 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import lit from "@astrojs/lit";
+
 // https://astro.build/config
 export default defineConfig({
   prefetch: {
     prefetchAll: true
   },
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false,
+  }), lit()],
 });
